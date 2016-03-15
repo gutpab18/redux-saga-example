2c5124ac7613f5d4cfea2952a518ec8aefc11dbf
Jacob Toye <jacob.toye@gmail.com>
2016-03-16 11:34:25 +1300

    Added Ava for testing and added initial Action tests.

--- test/actions/bookings.js
+++ test/actions/bookings.js
@@ -1,18 +1,59 @@
-// What are you testing?
-// What should it do?
-// What is the actual output?
-// What is the expected output?
-// How can the test be reproduced?
-
 import test from 'ava';
+import { actionTest } from 'redux-ava';
 
-// For each unit test you write,
-// answer these questions:
-test('What component aspect are you testing?', assert => {
-	const actual = 'What is the actual output?';
-	const expected = 'What is the expected output?';
+import * as Actions from '../../scripts/actions/bookings';
 
-	assert.equal(actual, expected, 'What should the feature do?');
+import {
+  START_POLL_BOOKINGS,
+  STOP_POLL_BOOKINGS,
+  FETCHED_BOOKINGS,
+  CANCEL_BOOKING,
+  CHECK_OUT_BOOKING,
+  DELETE_BOOKING,
+} from '../../scripts/constants';
 
-	assert.end();
-});
+const bookings = [{
+	id: 0,
+	plant: {
+		name: 'Smartrak Pool Car',
+		rego: 'EQB729',
+		make: 'Honda',
+		model: 'Accord',
+		year: '2008',
+	},
+	passengers: [],
+	journeyToLocation: 'Gone Burger',
+	returnToLocation: 'Smartrak Hamilton',
+	start: new Date(2016, 3, 15, 9, 0, 0),
+	end: new Date(2016, 3, 15, 11, 0, 0),
+	status: 'APPROVED',
+}];
+const testBookingId = bookings[0].id;
+
+test('startBookingsPoll action', actionTest(Actions.startBookingsPoll, {
+	type: START_POLL_BOOKINGS,
+}));
+
+test('stopBookingsPoll action', actionTest(Actions.stopBookingsPoll, {
+	type: STOP_POLL_BOOKINGS,
+}));
+
+test('receivedBookings action', actionTest(Actions.receivedBookings.bind(null, bookings), {
+	type: FETCHED_BOOKINGS,
+	bookings,
+}));
+
+test('cancelBooking action', actionTest(Actions.cancelBooking.bind(null, testBookingId), {
+	type: CANCEL_BOOKING,
+	id: testBookingId,
+}));
+
+test('checkOutBooking action', actionTest(Actions.checkOutBooking.bind(null, testBookingId), {
+	type: CHECK_OUT_BOOKING,
+	id: testBookingId,
+}));
+
+test('deleteBooking action', actionTest(Actions.deleteBooking.bind(null, testBookingId), {
+	type: DELETE_BOOKING,
+	id: testBookingId,
+}));
